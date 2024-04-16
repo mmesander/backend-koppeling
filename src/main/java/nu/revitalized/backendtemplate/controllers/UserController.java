9ecf85d409b806e5e472c61ce7b10c00e75daa24
Mark Mesander <mark_mesander@hotmail.com>
2024-04-16 15:40:08 +0200

    add UserController

---
+++ src/main/java/nu/revitalized/backendtemplate/controllers/UserController.java
@@ -0,0 +1,18 @@
+package nu.revitalized.backendtemplate.controllers;
+
+// Imports
+import nu.revitalized.backendtemplate.services.UserService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+}
