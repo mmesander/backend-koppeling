df15940c749230971da36ce2341c9a4e5d13732d
Mark Mesander <mark_mesander@hotmail.com>
2024-04-16 15:00:52 +0200

    add copy properties helper

---
+++ src/main/java/nu/revitalized/backendtemplate/helpers/CopyProperties.java
@@ -0,0 +1,10 @@
+package nu.revitalized.backendtemplate.helpers;
+
+// Imports
+import org.springframework.beans.BeanUtils;
+
+public class CopyProperties {
+    public static <Input, Output> void copyProperties(Input source, Output destination) {
+        BeanUtils.copyProperties(source, destination);
+    }
+}
