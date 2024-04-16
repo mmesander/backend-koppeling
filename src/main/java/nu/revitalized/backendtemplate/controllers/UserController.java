6afd0cd0882ca204a496db519c9257c49a08d1cc
Mark Mesander <mark_mesander@hotmail.com>
2024-04-16 15:45:44 +0200

    add get requests in controller

--- src/main/java/nu/revitalized/backendtemplate/controllers/UserController.java
+++ src/main/java/nu/revitalized/backendtemplate/controllers/UserController.java
@@ -1,10 +1,15 @@
 package nu.revitalized.backendtemplate.controllers;
 
 // Imports
+import jakarta.validation.Valid;
+import nu.revitalized.backendtemplate.dtos.input.UserInputDto;
+import nu.revitalized.backendtemplate.dtos.output.UserDto;
 import nu.revitalized.backendtemplate.services.UserService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin
 @RestController
@@ -14,5 +19,32 @@ public class UserController {
 
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    // ADMIN -- CRUD Requests
+    @GetMapping(value = "")
+    public ResponseEntity<List<UserDto>> getUsers() {
+        List<UserDto> dtos = userService.getUsers();
+
+        return ResponseEntity.ok().body(dtos);
+    }
+
+    @GetMapping(value = "/{username}")
+    public ResponseEntity<UserDto> getUser(
+            @PathVariable("username") String username
+    ) {
+        UserDto dto = userService.getUser(username);
+
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @GetMapping(value = "/search")
+    public ResponseEntity<List<UserDto>> searchUsers(
+            @RequestParam(required = false) String username,
+            @RequestParam(required = false) String email
+    ) {
+        List<UserDto> dtos = userService.getUsersByFilter(username, email);
+
+        return ResponseEntity.ok().body(dtos);
     }
 }
