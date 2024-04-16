9c138b63f66d960556040710775a9cc99037fd08
Mark Mesander <mark_mesander@hotmail.com>
2024-04-16 14:59:11 +0200

    add user service

---
+++ src/main/java/nu/revitalized/backendtemplate/services/UserService.java
@@ -0,0 +1,36 @@
+package nu.revitalized.backendtemplate.services;
+
+// Imports
+import lombok.Getter;
+import lombok.Setter;
+import nu.revitalized.backendtemplate.dtos.input.UserInputDto;
+import nu.revitalized.backendtemplate.dtos.output.UserDto;
+import nu.revitalized.backendtemplate.models.User;
+import nu.revitalized.backendtemplate.repositories.UserRepository;
+
+@Getter
+@Setter
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(
+            UserRepository userRepository
+    ) {
+        this.userRepository = userRepository;
+    }
+
+    // Transfer Methods
+    public User dtoToUser(UserInputDto inputDto) {
+        User user = new User();
+
+        user.setUsername(inputDto.getUsername().toLowerCase());
+        user.setPassword(inputDto.getPassword());
+        user.setEmail(inputDto.getEmail());
+
+        return user;
+    }
+
+    public UserDto userToDto(User user) {
+
+    }
+}
