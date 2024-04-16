68687f58be59f1cb5c2e19aeb2d50327eb97e3c3
Mark Mesander <mark_mesander@hotmail.com>
2024-04-16 14:09:58 +0200

    add model

---
+++ src/main/java/nu/revitalized/backendtemplate/models/User.java
@@ -0,0 +1,26 @@
+package nu.revitalized.backendtemplate.models;
+
+// Imports
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+    // Variables
+    @Id
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+}
