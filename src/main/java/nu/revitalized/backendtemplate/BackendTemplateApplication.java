e7cf297c32b7ddebbb4df8bf09b81bbf8d1977f2
Mark Mesander <mark_mesander@hotmail.com>
2024-04-16 12:56:07 +0200

    first commit

---
+++ src/main/java/nu/revitalized/backendtemplate/BackendTemplateApplication.java
@@ -0,0 +1,13 @@
+package nu.revitalized.backendtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendTemplateApplication.class, args);
+    }
+
+}
