669808e5338043c33471a5d373d0b9f044d90f46
Mark Mesander <mark_mesander@hotmail.com>
2024-05-09 17:14:55 +0200

    update checklist

--- src/main/java/nu/revitalized/backendtemplate/Checklist.java
+++ src/main/java/nu/revitalized/backendtemplate/Checklist.java
@@ -1,9 +1,4 @@
 package nu.revitalized.backendtemplate;
 
 public class Checklist {
-    //TODO Build security
-    //TODO Relation User - Authority in service
-    //TODO getUserAuthorities, assignAuthorityToUser, removeAuthorityFromUser
-    //TODO zorgen dat de laatste admin niet verwijderd kan worden
-    //TODO Zorgen dat als een gebruiker wordt aangemaakt dat hij de role user krijgtadd
 }
