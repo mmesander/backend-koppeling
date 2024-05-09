922bbc250aa4c7f97c0adda293ac605d0c17af0a
Mark Mesander <mark_mesander@hotmail.com>
2024-05-09 14:31:40 +0200

    add CustomUserDetailService and add relation with Authority in UserDto

--- src/main/java/nu/revitalized/backendtemplate/dtos/output/UserDto.java
+++ src/main/java/nu/revitalized/backendtemplate/dtos/output/UserDto.java
@@ -1,9 +1,13 @@
 package nu.revitalized.backendtemplate.dtos.output;
 
 // Imports
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Getter;
 import lombok.Setter;
 import nu.revitalized.backendtemplate.interfaces.IdentifiableUsername;
+import nu.revitalized.backendtemplate.models.Authority;
+
+import java.util.Set;
 
 @Getter
 @Setter
@@ -12,4 +16,8 @@ public class UserDto implements IdentifiableUsername {
     private String username;
     private String password;
     private String email;
+
+    // Relations
+    @JsonSerialize
+    private Set<Authority> authorities;
 }
