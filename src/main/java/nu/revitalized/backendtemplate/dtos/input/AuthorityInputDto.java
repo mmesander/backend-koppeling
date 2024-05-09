61732475b065f18d8723ebcac6e8fbb33d6370c5
Mark Mesander <mark_mesander@hotmail.com>
2024-05-09 14:55:41 +0200

    add AuthorityInputDto

---
+++ src/main/java/nu/revitalized/backendtemplate/dtos/input/AuthorityInputDto.java
@@ -0,0 +1,15 @@
+package nu.revitalized.backendtemplate.dtos.input;
+
+// Imports
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AuthorityInputDto {
+    @NotNull(message = "Authority is required")
+    @Pattern(regexp = "^ROLE_[a-zA-Z]+$", message = "Authority must start with ROLE_ and contain only letters")
+    private String authority;
+}
